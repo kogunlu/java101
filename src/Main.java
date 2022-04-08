2f2dfcc728b5c4ca35cf14e89d2a9cc780d873ad
kogunlu <k.ogunlu@gmail.com>
2022-04-08 12:21:04 +0300

    Some classes added.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.print("A sayısı giriniz:");
+        a = input.nextInt();
+        System.out.println(a);
+
+
+
+
+
+
+    }
+
+
+
+}
