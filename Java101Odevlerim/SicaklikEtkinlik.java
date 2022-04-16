4633aac8f9d84e467070fa83c80926be2156014f
kogunlu <k.ogunlu@gmail.com>
2022-04-16 22:04:43 +0300

    SicaklikEtkinlik added.

---
+++ Java101Odevlerim/SicaklikEtkinlik.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SicaklikEtkinlik {
+    public static void main(String[] args) {
+        /*Koşullar :
+
+Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+Ödev
+Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.*/
+
+        int hava;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Etkinlik planladığınız günün tahmini hava durumunu giriniz: ");
+            hava = input.nextInt();
+
+            if( hava < 5 ){
+                System.out.println("Kayak");
+            }else if ( (hava >= 5) && (hava < 15) ) {
+                System.out.println("Sinema");
+            }else if ( (hava >= 15) && (hava < 25) ) {
+                System.out.println("Piknik");
+            }else {
+                System.out.println("Yüzme");
+            }
+
+
+    }
+}
