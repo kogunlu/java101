0c92189d9f43d79c08f033fd7be8f662f8216a0e
kogunlu <k.ogunlu@gmail.com>
2022-04-20 13:10:53 +0300

    DortKatlari.java added.

---
+++ Java101Odevlerim/DortKatlari.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DortKatlari {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int i, sum=0;
+
+       do {
+           System.out.print("Bir sayı girin: ");
+            i = input.nextInt();
+
+                if (i % 4 == 0){
+                    sum = sum + i;
+                    System.out.println("Toplam: "+ sum);
+                }
+
+       }while ( i % 2 == 0 );
+
+        System.out.println("Program bitti.");
+    }
+}
