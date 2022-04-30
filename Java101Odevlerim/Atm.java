3859ff26cff1bc302cb80417706e2963103f806a
kogunlu <k.ogunlu@gmail.com>
2022-04-30 23:47:09 +0300

    Atm.java added.

---
+++ Java101Odevlerim/Atm.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+ class Atm {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int Try = 3;
+        String Username, Password;
+        int islemKodu, islemKodu2;
+        int accountBalance = 1000;
+        int tutar;
+
+
+
+
+        while (Try > 0) {
+            System.out.print("Kullanıcı adı giriniz: ");
+            Username = input.nextLine();
+
+            System.out.print("Şifrenizi girin: ");
+            Password = input.nextLine();
+
+            islemKodu = 0;
+
+            if (Username.equals("ogunlu") && Password.equals("findik")) {
+
+                while (islemKodu != 4){
+
+                System.out.println("Yapmak istediğiniz işlemi seçin\n1-Para Çekme\n2-Para Yatırma\n3-Ödeme\n4-Çıkış " +
+                        "Yap: ");
+                    islemKodu = input.nextInt();
+
+
+
+                        if (islemKodu == 1){
+                            System.out.print("Tutar giriniz: ");
+                            tutar = input.nextInt();
+
+                            accountBalance -= tutar;
+
+                            if(accountBalance >= 0){
+
+                                System.out.print("İşlem sonrası bakiyeniz: " + accountBalance);
+                                System.out.println("");
+
+
+                            }
+                            else {
+                                System.out.println("Bakiye yetersiz.");
+                                System.out.println("");
+                            }
+                        }
+
+                        if (islemKodu == 2){
+                            System.out.print("Tutar giriniz: ");
+                                tutar = input.nextInt();
+
+                            while (tutar < 0){
+                                System.out.print("Tutar negatif olamaz, lütfen geçerli tutar giriniz.");
+                                System.out.println("");
+                                System.out.print("Tutar giriniz: ");
+                                tutar = input.nextInt();
+                            }
+                                accountBalance += tutar;
+                            System.out.println("İşlem sonrası bakinyeiz: " + accountBalance);
+
+                            }
+                        if (islemKodu == 3) {
+                            System.out.println("1-Şans Oyunları\n2-Okul Harç Ödeme\n3-MTV Ödeme");
+                            islemKodu2 = input.nextInt();
+
+                        }
+
+                        if (islemKodu == 4){
+                            System.out.println("Çıkış başarılı.");
+                        }
+
+                        }
+                        Try = 0;
+
+                    }else {
+                Try--;
+                System.out.print("Hatalı giriş yaptınız, kalan deneme sayınız: " + Try);
+                System.out.println();
+                    if (Try ==0){
+                        System.out.println("Hesabınız bloke olmuştur.");
+                    }
+            }
+
+            }
+
+        }
+
+
+
+    }
+
