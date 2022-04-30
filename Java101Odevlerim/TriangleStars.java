de1bfce9a11dcb56d33c2a974280db9af39b4c21
kogunlu <k.ogunlu@gmail.com>
2022-04-30 22:27:58 +0300

    TriangleStars.java added

--- Java101Odevlerim/TriangleStars.java
+++ Java101Odevlerim/TriangleStars.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class StarWithNumber {
+public class TriangleStars {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
