6498e518e7fb9f1bb54ae96925c8ce2a2fd121b5
kogunlu <k.ogunlu@gmail.com>
2022-05-01 14:59:55 +0300

    TersUcgen.java added.

---
+++ Java101Odevlerim/TersUcgen.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Bir sayı giriniz: ");
+            int n = input.nextInt();
+
+        for(int i=n; i>= 0; i--){
+
+
+            for(int k=1; k<=(n-i);k++){
+
+                System.out.print(" ");
+            }
+
+
+            for (int j=1; j<=(2*i-1) ; j++){
+
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
+
+    }
+}
