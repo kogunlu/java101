b1ac9b3de1c3b63f99417441218cfc916dfa9362
kogunlu <k.ogunlu@gmail.com>
2022-05-01 16:55:06 +0300

    AsalSayilar.java added.

---
+++ Java101Odevlerim/AsalSayilar.java
@@ -0,0 +1,33 @@
+public class AsalSayilar {
+    public static void main(String[] args) {
+
+
+int i, k;
+
+        for (i=1; i <= 100; i ++){
+
+            int sayac=0;
+
+            for (k=2; k < i; k++ ){
+
+                if ( i % k == 0){
+
+                    sayac = 1;
+
+                }
+
+
+            }
+
+            if (sayac == 0)
+            {
+            System.out.print(i + " ");
+            }
+
+
+        }
+
+
+
+    }
+}
