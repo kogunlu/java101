5fcea942fbedb9ce41aa375ebc75f23c8cebfc07
kogunlu <k.ogunlu@gmail.com>
2022-05-04 21:19:23 +0300

    AsalSayiV2.java added.

---
+++ Java101Odevlerim/AsalSayiV2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class AsalSayiV2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+            int a = input.nextInt();
+
+            if(asal(a) == true){
+                System.out.println("Sayı asaldır");
+            }else{
+                System.out.println("Asal değildir.");
+            }
+    }
+    static boolean asal(int n){
+
+        boolean result = true;
+
+        if(n==2){
+            result = true;
+        }
+        for (int i = 2; i < n; i++ ){
+            if (n % i == 0){
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+}
