ea3c2c0dc37f379f4bb5a0da88a79e71ca8f27ad
kogunlu <k.ogunlu@gmail.com>
2022-05-09 22:27:16 +0300

    NearNumber.java added.

---
+++ Java101Odevlerim/Java101/NearNumber.java
@@ -0,0 +1,35 @@
+package Java101;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NearNumber {
+    public static void main(String[] args) {
+
+        int[] list = {-12, -120, 50, 25, 9, 11};
+        Arrays.sort(list);
+        System.out.println("Küçükten büyüğe sıralama");
+        System.out.println(Arrays.toString(list));
+
+        Scanner input = new Scanner(System.in);
+        int diziUzunlugu = list.length - 1;
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = input.nextInt();
+
+        int nearBig = 0;
+        int nearSmall = 0;
+        int k = 0;
+
+        for (int i = 0; i < list.length; i++){
+
+       //   System.out.println(list[i]); > Dönen sayıları görmek için yazıldı.
+
+            if (sayi < list[i]){
+                System.out.println("Girilen sayıdan büyük ilk sayı: " + list[i]);
+                System.out.println("Girilen sayıdan küçük ilk sayı: " + list[i-1]);
+                break;
+            }
+
+
+        }
+    }
+}
