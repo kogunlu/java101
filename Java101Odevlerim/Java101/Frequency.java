24e751d02a24ff0cd17256744a52ee36b3d3bfef
kogunlu <k.ogunlu@gmail.com>
2022-05-13 08:06:21 +0300

    Frequency.java added.

---
+++ Java101Odevlerim/Java101/Frequency.java
@@ -0,0 +1,33 @@
+package Java101;
+import java.util.Arrays;
+public class Frequency {
+
+    public static void main(String[] args) {
+
+        int[] dizi={10,20,20,10,10,20,5,20};
+        int n=1;
+        int a=0;
+        Arrays.sort(dizi);          //5 10 10 10 20 20 20 20
+
+
+
+        for(int i=0;i< dizi.length;i++){
+            if(dizi[i]!=a){
+
+
+                for(int j=0;j<dizi.length;j++){
+
+                    if ((i!=j) && (dizi[i]==dizi[j])) {
+                        n++;
+                        a=dizi[i];
+                    }
+                }
+                System.out.println(dizi[i] +" sayısı " + n + " kere tekrar edildi.");
+                n=1;
+
+            }
+        }
+    }
+}
+
+
