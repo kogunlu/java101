d4fd33f1d34dbda60061ac5cfc5b411cb65b5fdf
Kuday Ogunlu <k.ogunlu@gmail.com>
2022-06-07 13:17:50 +0300

    Odev_10.sql added.

---
+++ Java101Odevlerim/SQL_Odevleri/Odev_10.sql
@@ -0,0 +1,15 @@
+Odev_10
+
+1st part
+SELECT city, country FROM City
+LEFT JOIN country ON city.country_id = country.country_id
+ORDER BY country;
+
+2nd part
+SELECT payment_id, first_name, last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id
+ORDER BY first_name;
+
+SELECT rental_id, first_name, last_name FROM rental
+FULL JOIN customer ON customer.customer_id = rental.customer_id
+ORDER BY first_name;
